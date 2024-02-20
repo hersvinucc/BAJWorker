3fc32cc177be0f734064959dbfc7e5ce1e85df37
Jiro <20200097m.ibia.jiroallen.bscs@gmail.com>
2024-02-20 21:10:56 +0800

    [Two Steps Sign Up] Initial Design

--- app/src/main/java/com/example/bookajobworker/SignIn.kt
+++ app/src/main/java/com/example/bookajobworker/SignIn.kt
@@ -7,18 +7,30 @@ import android.widget.Button
 
 class SignIn : AppCompatActivity() {
     private lateinit var btnBackToLogin: Button
+    private lateinit var btnContinue: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
         //For Login Button
-        btnBackToLogin = findViewById(R.id.buttonSignIn)
+        btnBackToLogin = findViewById(R.id.buttonBackToLogin)
         btnBackToLogin.setOnClickListener {
             navToLogin()
+        }
+        // For Continue Button
+        btnContinue = findViewById(R.id.buttonContinue)
+        btnContinue.setOnClickListener {
+            navToNextStep()
         }
 
 
 
+    }
+
+    private fun navToNextStep() {
+        val intent = Intent(this, SignUpContinue::class.java)
+        startActivity(intent)
+        finish()
     }
 
     private fun navToLogin() {
---
+++ app/src/main/java/com/example/bookajobworker/SignUpContinue.kt
@@ -0,0 +1,11 @@
+package com.example.bookajobworker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpContinue : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_personal)
+    }
+}
