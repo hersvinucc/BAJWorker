ec890f1309111a6c9176acbabf73bec4a857f522
hersvinucc <20200098m.labastida.hersvin.bscs@gmail.com>
2024-02-20 22:45:12 +0800

    new push

--- app/src/main/java/com/example/bookajobworker/SignUpContinue.kt
+++ app/src/main/java/com/example/bookajobworker/SignUpContinue.kt
@@ -2,10 +2,68 @@ package com.example.bookajobworker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.Spinner
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+import android.app.DatePickerDialog
+import java.util.*
 class SignUpContinue : AppCompatActivity() {
+
+    private lateinit var editBirthDate: EditText
+    private lateinit var datePicker: DatePicker
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in_personal)
+
+        val genderSpinner: Spinner = findViewById(R.id.genderSignIn)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.gender_options,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            genderSpinner.adapter = adapter
+        }
+
+        editBirthDate = findViewById(R.id.birthdateSignIn)
+        datePicker = findViewById(R.id.datePicker)
+
+        editBirthDate.setOnClickListener {
+            showDatePickerDialog()
+        }
+
+        datePicker.init(
+            Calendar.getInstance().get(Calendar.YEAR),
+            Calendar.getInstance().get(Calendar.MONTH),
+            Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        ) { view, year, monthOfYear, dayOfMonth ->
+            val selectedDate = Calendar.getInstance()
+            selectedDate.set(year, monthOfYear, dayOfMonth)
+            val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+            editBirthDate.setText(sdf.format(selectedDate.time))
+        }
+    }
+
+    //Dialog Show
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val datePickerDialog = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                val selectedDate = Calendar.getInstance()
+                selectedDate.set(year, monthOfYear, dayOfMonth)
+                val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+                editBirthDate.setText(sdf.format(selectedDate.time))
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
     }
 }
